45b302aec0996946cd4fd644e9dc29de695499e4
dbisu <dabailey@gmail.com>
2021-06-16 10:02:24 -0500

    Better fix for issue 1

--- duckyinpython.py
+++ duckyinpython.py
@@ -70,7 +70,7 @@ print("Running payload.dd")
 previousLine = ""
 duckyScript = f.readlines()
 for line in duckyScript:
-    line = line.replace('\r','')
+    line = line.rstrip()
     if(line[0:6] == "REPEAT"):
         for i in range(int(line[7:])):
             #repeat the last command
