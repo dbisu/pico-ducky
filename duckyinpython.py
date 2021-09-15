fcafd51f7f6ba322550750d4fee1d2dec5a38a64
mexifivehd <89155311+mexifivehd@users.noreply.github.com>
2021-09-15 20:53:31 +0200

    Update duckyinpython.py

--- duckyinpython.py
+++ duckyinpython.py
@@ -1,6 +1,6 @@
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keyboard_layout_DE import KeyboardLayoutDE
 from adafruit_hid.keycode import Keycode
 import time
 import digitalio
