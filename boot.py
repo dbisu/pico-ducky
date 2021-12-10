0ba87b404fe8a781b870273dff5fc0ba82617fdb
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2021-12-10 07:59:43 +0330

    Defaults the USB to stealth mode and disables everything except HID devices.

--- boot.py
+++ boot.py
@@ -1,16 +1,24 @@
 from board import *
 import digitalio
 import storage
+import usb_midi
+import time
+led = digitalio.DigitalInOut(LED)
+led.direction = digitalio.Direction.OUTPUT
+storage.remount("/", True)
 
 noStorageStatus = False
 noStoragePin = digitalio.DigitalInOut(GP15)
 noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
-noStorageStatus = not noStoragePin.value
 
-if(noStorageStatus == True):
-    # don't show USB drive to host PC
+if(noStoragePin.value == True):
+    # boot as stealth mode
     storage.disable_usb_drive()
+    usb_cdc.disable()
+    usb_midi.disable()
+    usb_cdc.enable(console=False, data=False)
     print("Disabling USB drive")
 else:
     # normal boot
-    print("USB drive enabled")
+    storage.unmount()
+    print("Enabling USB drive")
