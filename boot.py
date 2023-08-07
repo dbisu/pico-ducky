8c49ecc9ebc309310db7f8a4b50939bac897ae2f
dontknowmaedawda <141620311+dontknowmaedawda@users.noreply.github.com>
2023-08-07 16:21:09 +0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -16,22 +16,6 @@ noStorageStatus = noStoragePin.value
 # If GP15 is not connected, it will default to being pulled high (True)
 # If GP is connected to GND, it will be low (False)
 
-# Pico:
-#   GP15 not connected == USB visible
-#   GP15 connected to GND == USB not visible
-
-# Pico W:
-#   GP15 not connected == USB NOT visible
-#   GP15 connected to GND == USB visible
-
-if(board.board_id == 'raspberry_pi_pico'):
-    # On Pi Pico, default to USB visible
-    noStorage = not noStorageStatus
-elif(board.board_id == 'raspberry_pi_pico_w'):
-    # on Pi Pico W, default to USB hidden by default
-    # so webapp can access storage
-    noStorage = noStorageStatus
-
 if(noStorage == True):
     # don't show USB drive to host PC
     storage.disable_usb_drive()
